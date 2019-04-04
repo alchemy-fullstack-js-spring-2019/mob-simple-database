58d5f228da776e04961f6cfaadbd40a9df901c8b
Mallarie <mallariemilano@gmail.com>
2019-04-04 14:48:24 -0700

    code cleaned up

--- test/store.test.js
+++ test/store.test.js
@@ -1,13 +1,10 @@
 const Store = require('../lib/store');
 const path = require('path');
-const fs = require('fs');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 
-console.log(Store, mkdirp, rimraf);
 describe('Store() thing', () => {
   const rootDir = path.join(__dirname, 'database');
-  console.log(rootDir);
   const db = new Store(rootDir);
   beforeEach(done => {
     mkdirp(rootDir, done); 
