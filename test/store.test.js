54229df3c9d9e7e915916a5d4d1b1bfc997809bf
emily baier <emilyrbaier@gmail.com>
2019-04-04 08:38:40 -0700

    working on it

---
+++ test/store.test.js
@@ -0,0 +1,21 @@
+const Store = require('../lib/store');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+
+describe('store database', () => {
+    let testStore = null;
+    beforeEach(done => {
+        //mkdirp('./data', done)
+        testStore = new Store();
+    });
+    afterEach(() => {
+        //delete directory rimraf
+    });
+    it('create method makes id property for the object and saves to a file', () => {
+        testStore.create({ name: 'bob', species: 'sponge' }, (err, savedAnimalWithId) => {
+            if(err) throw err;
+            expect(savedAnimalWithId.name).toEqual('bob');
+
+        });
+    });
+});
