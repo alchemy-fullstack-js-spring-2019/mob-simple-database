4637875fdc4938e4de0cbe21fc8e07abe6bac5b9
Parker Helland <hellandp@gmail.com>
2019-04-04 11:19:37 -0700

    create function written, tests passed

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,10 @@
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 
-const Store = require('../lib/index.js');
+const { Store } = require('../lib/index.js');
 
 describe('', () => {
+  const store = new Store('./lib/danimals');
   beforeEach((done) => {
     mkdirp('./lib/danimals', done);
   });
@@ -12,7 +13,12 @@ describe('', () => {
     rimraf('./lib/danimals', done);
   });
 
-  it('create new json file', () => {
-    // expect
+  it('create new json file', (done) => {
+    const obj = { cat: 'kitty' };
+    store.create(obj, (err, newObj) => {
+      if(err) throw err;
+      expect(newObj._id).toEqual(expect.any(String));
+      done();
+    });
   });
 });
