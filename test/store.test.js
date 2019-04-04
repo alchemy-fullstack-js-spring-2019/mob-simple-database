a11516c66c62453e9bdd102d709582b79a29dbf2
Tommy Tran <tranttommy95@gmail.com>
2019-04-04 09:58:45 -0700

    create just returns new object with id

--- test/store.test.js
+++ test/store.test.js
@@ -15,4 +15,18 @@ describe('Store Class', () => {
     const store = new Store(rootDirectory);
     expect(store.rootDirectory).toEqual(rootDirectory);
   });
+
+  it('create makes obj with id', done => {
+    const obj = {
+      name: 'guy',
+      age: 13
+    };
+    const store = new Store(rootDirectory);
+    store.create(obj, (err, objSaved) => {
+      expect(err).toBeFalsy();
+      expect(objSaved.name).toEqual(obj.name);
+      expect.any(objSaved._id);
+      done();
+    });
+  });
 });
