c10d528cab0ea922cf018fefde11e8397883c749
Tommy Tran <tranttommy95@gmail.com>
2019-04-04 10:09:35 -0700

    create can write file, test passed

--- test/store.test.js
+++ test/store.test.js
@@ -1,9 +1,12 @@
 const Store = require('../lib/store.js');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
+const fs = require('fs');
+const path = require('path');
 
 describe('Store Class', () => {
   const rootDirectory = './root';
+  const options = { encoding: 'utf8' };
   beforeEach(done => {
     mkdirp(rootDirectory, done);
   });
@@ -27,6 +30,23 @@ describe('Store Class', () => {
       expect(objSaved.name).toEqual(obj.name);
       expect.any(objSaved._id);
       done();
+    });
+  });
+
+  it('create writes to root/id', done => {
+    const obj = {
+      name: 'guy',
+      age: 13
+    };
+    const store = new Store(rootDirectory);
+    store.create(obj, (err, objSaved) => {
+      expect(err).toBeFalsy();
+      const filePath = path.join(rootDirectory, objSaved._id);
+      fs.readFile(filePath, options, (err, data) => {
+        expect(err).toBeFalsy();
+        expect(data).toEqual(JSON.stringify(objSaved));
+        done();
+      });
     });
   });
 });
