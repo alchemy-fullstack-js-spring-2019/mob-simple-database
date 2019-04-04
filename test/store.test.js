ca2b703ab9759ae2850ef4d15c974555206beceb
Tommy Tran <tranttommy95@gmail.com>
2019-04-03 17:31:29 -0700

    created beforeEach and afterEach

--- test/store.test.js
+++ test/store.test.js
@@ -1,7 +1,17 @@
 const Store = require('../lib/store.js');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+
 describe('Store Class', () => {
+  const rootDirectory = './root';
+  beforeEach(done => {
+    mkdirp(rootDirectory, done);
+  });
+  afterEach(done => {
+    rimraf(rootDirectory, done);
+  });
+
   it('has property path', () => {
-    const rootDirectory = './root';
     const store = new Store(rootDirectory);
     expect(store.rootDirectory).toEqual(rootDirectory);
   });
