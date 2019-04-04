79eeb7317812697d4552bb6af9386d814a46e1e2
heather <unphurlng@gmail.com>
2019-04-04 10:41:57 -0700

    made directory created object passed test

--- test/simple-db.test.js
+++ test/simple-db.test.js
@@ -1,16 +1,33 @@
-const rimraf = require('rmraf');
+const rimraf = require('rimraf');
 const mkdirp = require('mkdirp');
+const path = require('path');
+const Store = require('../lib/simple-db');
+const rootDirectory = path.join(__dirname, 'animals');
 
 describe('Store', () => {
   beforeEach(done => {
-    mkdirp('./data', done);
+    mkdirp(rootDirectory, done);
   });
-
+  let store = null;
+  beforeEach(() => {
+    store = new Store(rootDirectory);
+  });
   afterEach(done => {
-    rimraf('./data', done);
+    rimraf(rootDirectory, done);
+  });
+  
+  it('has a root dir', () => {
+    expect(store.rootDir).toEqual(rootDirectory);
   });
 
-  it('can create a new json file', () => {
-
+  it('creates an object in our store', done => {
+    const snake = {
+      type: 'cobra'
+    };
+    store.create(snake, (err, createdSnake) => {
+      expect(createdSnake).toEqual({ ...snake, _id: expect.any(String) });
+      done();
+    });
   });
+  
 });
