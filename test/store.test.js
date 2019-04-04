26dbdd5ed937cd2e4d6dc4c5c6e1e9d444a20231
emily baier <emilyrbaier@gmail.com>
2019-04-04 09:54:14 -0700

    id functionality in create method and downloaded mkdirp and rimraf

--- test/store.test.js
+++ test/store.test.js
@@ -3,19 +3,21 @@ const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 
 describe('store database', () => {
+
     let testStore = null;
     beforeEach(done => {
-        //mkdirp('./data', done)
+        mkdirp('./data', done);
         testStore = new Store();
     });
-    afterEach(() => {
-        //delete directory rimraf
+
+    afterEach(done => {
+        rimraf('./data', done);
     });
+
     it('create method makes id property for the object and saves to a file', () => {
         testStore.create({ name: 'bob', species: 'sponge' }, (err, savedAnimalWithId) => {
             if(err) throw err;
             expect(savedAnimalWithId.name).toEqual('bob');
-
         });
     });
 });
