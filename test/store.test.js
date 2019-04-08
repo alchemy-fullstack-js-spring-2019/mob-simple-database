b85b88a41388688c517ae61aaa0938b1f38604c0
Masmics <cherlocker3113@gmail.com>
2019-04-07 19:05:26 -0700

    all tests passing

--- test/store.test.js
+++ test/store.test.js
@@ -1,48 +1,79 @@
-const Store = require('../lib/store');
-const path = require('path');
-const mkdirp = require('mkdirp');
-const rimraf = require('rimraf');
+const MemoryDatabase = require('../lib/store');
 
-describe('Store() thing', () => {
-  const rootDir = path.join(__dirname, 'database');
-  const db = new Store(rootDir);
-  beforeEach(done => {
-    mkdirp(rootDir, done); 
+describe('MemoryDatabase', () => {
+  let db  = null;
+  beforeEach(() => {
+    db = new MemoryDatabase();
   });
-  afterEach(done => {
-    rimraf(rootDir, done);
+
+  it('creates an obj in db', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+    expect(createdCat.name).toEqual('fluffy');
   });
-  beforeEach(done => {
-    const pets = {
-      name: 'Mister',
-      age: 8
-    };
-    db.create(pets, done);
+
+  it('can find an obj by id in the db', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+
+    const foundCat = db.findById(createdCat._id);
+    expect(foundCat).toEqual(createdCat);
   });
-  describe('create method', () => {
-    it('checks that obj passed to .create() has uuid', done => {
-      const pets = {
-        name: 'Mister',
-        age: 8
-      };
-      db.create(pets, (err, newPet) => {
-        expect(newPet._id).toEqual(expect.any(String));
-        done(err);
-      });
-    });
+
+  it('throws error when obj does not exist', () => {
+    expect(() => {
+      db.findById('notARealId');
+    }).toThrowError('No object with _id notARealId');
   });
-  describe('find by ID', () => {
-    it('finds an obj by id', () => {
-      const pets = {
-        name: 'Mister',
-        age: 8
-      };
-      db.create(pets, (err, newPet) => {
-        db.findById(newPet._id, (err, objectFromFile) => {
-          if(err) throw err;
-          expect(newPet).toEqual(objectFromFile);
-        });
-      });
-    });
+
+  it('lists all objects in the database', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+    const dog = { name: 'perro' };
+    const createdDog = db.create(dog);
+
+    const allItems = db.find();
+    expect(allItems).toEqual([createdCat, createdDog]);
+  });
+
+  it('can find an obj by id in the db and replace it with another', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+    const catB = { name: 'thatcat' };
+    const createdCatB = db.create(catB);
+  
+    const foundCat = db.findByIdAndUpdate(createdCat._id, createdCatB);
+    console.log('store', db.find());
+    expect(foundCat).toEqual(createdCatB);
+  });
+
+  it('throws error when obj does not exist', () => {
+    expect(() => {
+      db.findByIdAndUpdate('notARealId');
+    }).toThrowError('No object with _id notARealId');
+  });
+
+  it('can find an object by id and delete it', () => {
+    const cat = { name: 'fluffy' };
+    const createdCat = db.create(cat);
+    const deletedCat = db.findByIdAndDelete(createdCat._id);
+    expect(deletedCat).toEqual({ deleted: 1 });
+  });
+
+  it('throws error when obj does not exist', () => {
+    expect(() => {
+      db.findByIdAndDelete('notARealId');
+    }).toThrowError('No object with _id notARealId');
+  });
+
+  it('can delete all objects in a database', () => {
+    const cat = { name: 'fluffy' };
+    db.create(cat);
+    const dog = { name: 'dawg' };
+    db.create(dog);
+
+    db.drop();
+    expect(db.drop()).toBeFalsy();
   });
 });
+
